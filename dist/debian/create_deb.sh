e58fe219e95b56ed58f7a25bd72802db54a1bd24
Hackrylix <hackrylix@itsworking.be>
2013-05-04 20:52:00 +0200

    fix path and loop

--- dist/debian/create_deb.sh
+++ dist/debian/create_deb.sh
@@ -4,18 +4,14 @@ echo "Creating Debian's package..."
 nano ./liquidprompt/DEBIAN/control
 
 echo "Copying files..."
-if [ ! -d ./liquidprompt/usr/bin/ ]
-then
-	mkdir -p ./liquidprompt/usr/bin/
-fi
 if [ ! -d ./liquidprompt/etc/profile.d/ ]
 then
 mkdir -p ./liquidprompt/etc/profile.d/
 fi
-cp ../../liquidprompt ./liquidprompt/usr/bin/liquidprompt
-cp ../../liquidpromptrc ./liquidprompt/etc/profile.d/liquidpromptrc.sh
-chmod a+x ./liquidprompt/usr/bin/liquidprompt
-chmod a+x ./liquidprompt/etc/profile.d/liquidpromptrc.sh
+cp ../../liquidprompt.sh ./liquidprompt/etc/profile.d/liquidprompt.sh
+cp ../../liquidpromptrc ./liquidprompt/etc/liquidpromptrc
+
+chmod a+x ./liquidprompt/etc/profile.d/liquidprompt.sh
 
 echo "Building liquidprompt.deb..."
 dpkg-deb -b liquidprompt
