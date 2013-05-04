92dcad0f1144952a004323a8fa5a7ca8edd25112
Hackrylix <hackrylix@itsworking.be>
2013-05-04 19:21:09 +0200

    release deb

--- dist/debian/create_deb.sh
+++ dist/debian/create_deb.sh
@@ -4,15 +4,19 @@ echo "Creating Debian's package..."
 nano ./liquidprompt/DEBIAN/control
 
 echo "Copying files..."
+if [ ! -d ./liquidprompt/usr/bin/ ]
+then
+	mkdir -p ./liquidprompt/usr/bin/
+fi
+if [ ! -d ./liquidprompt/etc/ ]
+then
+	mkdir -p ./liquidprompt/etc/
+fi
 cp ../../liquidprompt ./liquidprompt/usr/bin/liquidprompt
-cp ../../liquidpromptrc-dist ./liquidprompt/etc/liquidpromptrc
+cp ../../liquidpromptrc ./liquidprompt/etc/liquidpromptrc
 chmod a+x ./liquidprompt/usr/bin/liquidprompt
 
 echo "Building liquidprompt.deb..."
 dpkg-deb -b liquidprompt
-
-echo "Deleting files..."
-rm -f ./liquidprompt/etc/*
-rm -f ./liquidprompt/usr/bin/*
 
 echo "Done !"
